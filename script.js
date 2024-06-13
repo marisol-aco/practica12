cb54692f507d21587e02b6a17fe21927dbaab5c2
sol <solacosta619@gmail.com>
2024-06-13 01:49:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var numero01;
+var numero02;
+numero01=parseInt(prompt("Ingrese el primer numero 1",""));
+numero01=parseInt(prompt("Ingrese el primer numero 2",""));
+if(numero01 > numero02)
+{
+  document.write("El numero mayor es:" + numero01 +"(Numero 1 )");
+}
+else
+{
+  document.write("El numero mayor es:" + numero02 +"(Numero 2 )");
+}
